1b2cbc24bed52ac62f4379958f67a1c895a72cb8
Mareedu Saibabu <a3097640@bestbuy.com>
2025-10-01 12:40:05 +0530

    backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,11 @@
+import { User } from "@qeetro/types";
 
+const user: User = {
+  id: "1",
+  name: "John Doe",
+  email: "john.doe@example.com",
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+};
+
+console.log(user);
