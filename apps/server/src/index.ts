439c2c0922932b0b92f0cc049bc9907274811bb9
Mareedu Saibabu <a3097640@bestbuy.com>
2025-12-22 07:02:14 +0530

    prettier

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,15 +1,15 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
-const HOST = process.env?.['SERVER_HOST'] || 'localhost';
-const PORT = Number(process.env?.['SERVER_PORT']) || 4001;
+const HOST = process.env?.["SERVER_HOST"] || "localhost";
+const PORT = Number(process.env?.["SERVER_PORT"]) || 4001;
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/health', (_: Request, res: Response) => {
-  res.send({ status: 'ok' });
+app.get("/health", (_: Request, res: Response) => {
+  res.send({ status: "ok" });
 });
 
 app.listen(PORT, HOST, () => {
--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from '@/components/app-sidebar';
+import { AppSidebar } from "@/components/app-sidebar";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,9 +6,9 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
-import { Separator } from '@/components/ui/separator';
-import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Dashboard() {
   return (
